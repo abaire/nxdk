b8fc2a5be10a00aeb8f64b9f77ddc68bb297553c
Antonio Abbatangelo <antonio.abbatangelo@gmail.com>
2023-02-26 21:00:13 -0500

    pbkit: Support setting FB size multiplier

--- lib/pbkit/pbkit.c
+++ lib/pbkit/pbkit.c
@@ -165,6 +165,7 @@ static  DWORD           pb_FBVFlag;
 static  DWORD           pb_GPUFrameBuffersFormat;//encoded format for GPU
 static  DWORD           pb_EXAddr[8];       //extra buffers addresses
 static  DWORD           pb_ExtraBuffersCount=0;
+static  DWORD           pb_FBSizeMultiplier = 1;
 
 static  DWORD           pb_DepthStencilAddr;
 static  DWORD           pb_DepthStencilPitch;
@@ -2633,6 +2634,11 @@ void pb_set_color_format(unsigned int fmt, bool swizzled) {
     assert(swizzled == false);
 }
 
+void pb_set_fb_size_multiplier(unsigned int multiplier) {
+    assert(multiplier > 0);
+    pb_FBSizeMultiplier = multiplier;
+}
+
 int pb_init(void)
 {
     DWORD           old;
@@ -2741,7 +2747,6 @@ int pb_init(void)
     pb_PutRunSize=0;
 
     pb_FrameBuffersAddr=0;
-
 
     pb_DmaBuffer8=MmAllocateContiguousMemoryEx(32,0,MAXRAM,0,4);
     pb_DmaBuffer2=MmAllocateContiguousMemoryEx(32,0,MAXRAM,0,4);
@@ -3393,7 +3398,7 @@ int pb_init(void)
         }
     }
 
-    Size=Pitch*VSize;
+    Size=Pitch*VSize*pb_FBSizeMultiplier;
 
     //verify 64 bytes alignment for size of a frame buffer
     if (Size&(64-1)) debugPrint("pb_init: FBSize is not well aligned.\n");
@@ -3466,7 +3471,7 @@ int pb_init(void)
         }
     }
 
-    Size=Pitch*VSize;
+    Size=Pitch*VSize*pb_FBSizeMultiplier;
 
     //verify 64 bytes alignment for size of a frame buffer
     if (Size&(64-1)) debugPrint("pb_init: DSSize is not well aligned.\n");
@@ -3520,7 +3525,7 @@ int pb_init(void)
             }
         }
 
-        Size=Pitch*VSize;
+        Size=Pitch*VSize*pb_FBSizeMultiplier;
 
         //verify 64 bytes alignment for size of a frame buffer
         if (Size&(64-1)) debugPrint("pb_init: EXSize is not well aligned.\n");
