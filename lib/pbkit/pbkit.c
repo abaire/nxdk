3598c12594b99f30533a48e084e704d692dde333
Erik Abair <erik.abair@gmail.com>
2023-05-30 21:16:41 -0700

    Increase DMA limit for antialiasing tests.

--- lib/pbkit/pbkit.c
+++ lib/pbkit/pbkit.c
@@ -1861,7 +1861,8 @@ static void set_draw_buffer(DWORD buffer_addr)
 
     //DMA channel 9 is used by GPU in order to render pixels
     dma_addr=buffer_addr;
-    dma_limit=height*pitch-1; //(last byte)
+#warning NXDK_PGRAPH_TESTS: RESERVING 4x SIZE FOR ANTIALIASING TESTS
+    dma_limit=height*pitch*4-1; //(last byte)
     dma_flags=DMA_CLASS_3D|0x0000B000;
     dma_addr|=3;
 
@@ -1880,7 +1881,8 @@ static void set_draw_buffer(DWORD buffer_addr)
 
     //DMA channel 11 is used by GPU in order to bitblt images
     dma_addr=buffer_addr;
-    dma_limit=height*pitch-1; //(last byte)
+#warning NXDK_PGRAPH_TESTS: RESERVING 4x SIZE FOR ANTIALIASING TESTS
+    dma_limit=height*pitch*4-1; //(last byte)
     dma_flags=DMA_CLASS_3D|0x0000B000;
     dma_addr|=3;
 
@@ -1905,7 +1907,8 @@ static void set_draw_buffer(DWORD buffer_addr)
         if (depth_stencil)
         {
             dma_addr=pb_DSAddr&0x03FFFFFF;
-            dma_limit=height*pitch_depth_stencil-1; //(last byte)
+#warning NXDK_PGRAPH_TESTS: RESERVING 4x SIZE FOR ANTIALIASING TESTS
+            dma_limit=height * pitch_depth_stencil * 4 - 1;  //(last byte)
             dma_flags=DMA_CLASS_3D|0x0000B000;
             dma_addr|=3;
             flag=1;
@@ -1935,7 +1938,7 @@ static void set_draw_buffer(DWORD buffer_addr)
         p=pb_push1(p,NV20_TCL_PRIMITIVE_3D_STENCIL_ENABLE,1);   //StencilEnable=TRUE
         pb_end(p);
 
-        pb_DepthStencilLast=depth_stencil;
+      pb_DepthStencilLast = depth_stencil;
     }
 
     p=pb_begin();
