497500579939542b1ba99e1d0d21a5154bd04c3b
Erik Abair <erik.abair@gmail.com>
2025-03-05 07:06:07 -0800

    DONOTSUBMIT: Replaces hello sample with an xlaunchxbeex test.

--- samples/hello/main.c
+++ samples/hello/main.c
@@ -1,10 +1,30 @@
 #include <hal/debug.h>
 #include <hal/video.h>
+#include <hal/xbox.h>
 #include <windows.h>
 
+
+
+//asdfasdf
 int main(void)
 {
     XVideoSetMode(640, 480, 32, REFRESH_DEFAULT);
+
+    struct LaunchData00000001 {
+        uint32_t reason;
+        uint32_t context;
+        uint32_t parameters[2];
+        uint8_t padding[3072 - 16];
+    };
+
+    struct LaunchData00000001 launch_data = {
+        6,
+        0,
+        {0, 0},
+        {0}
+     };
+
+    XLaunchXBEEx(NULL, &launch_data);
 
     while(1) {
         debugPrint("Hello nxdk!\n");
